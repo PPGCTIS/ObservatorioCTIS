c46100847229201f3613eebbcb611720c7ff7478
Arthur de Sá Ferreira <arthur_sf@icloud.com>
2026-06-18 14:24:59 -0300

    Commit.

--- Scripts/fonte-capes.R
+++
@@ -1,8 +0,0 @@
-if (knitr::is_html_output()){
-  cat('\n\n')
-}
-cat('**Fontes**: [**CAPES**](https://www.gov.br/capes/pt-br)')
-cat('\n\n')
-if (knitr::is_html_output()) {
-  cat('<br><a style="float:right" href="#top"><b>Início &nbsp;</b>', fontawesome::fa('circle-arrow-up'), '</a><br>')
-}
